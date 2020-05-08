a774085252da1b04187cb7f9c4188e40a3abeb51
AhmedAlsum <ahmedalsum2@gmail.com>
2020-05-09 00:37:59 +0200

    get onetime and recurring fees

---
+++ BillingModule/billingModule/src/SystemObjects/One_Time.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SystemObjects;
+
+/**
+ *
+ * @author ahmed
+ */
+public class One_Time {
+    
+    public int one_time_id;
+    public String osname ;
+    public double osfees;
+
+    public One_Time(int one_time_id, String osname, double osfees) {
+        this.one_time_id = one_time_id;
+        this.osname = osname;
+        this.osfees = osfees;
+    }
+    
+    
+    
+}
